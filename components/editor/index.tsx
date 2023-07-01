f29fae408569dd7bae8581e98042aae34f945c13
Ankur-Datta-4 <ankurr2002@gmail.com>
2023-07-01 09:55:31 +0530

    [ADD] auto-save

--- components/editor/index.tsx
+++ components/editor/index.tsx
@@ -6,31 +6,73 @@ import { TiptapEditorProps } from "./props";
 import { EditorBubbleMenu } from "./components";
 import useBlog from "@/lib/hooks/use-blog";
 import LoadingCircle from "../ui/loading-circle";
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
 
 interface TipTapEditorProps {
   selectedBlog: Blog | null;
   userSlug: string;
+  saved: Boolean;
+  setSaved: any;
+  setEdited: any;
 }
 
-const Tiptap = ({ selectedBlog, userSlug }: TipTapEditorProps) => {
+const Tiptap = ({
+  selectedBlog,
+  userSlug,
+  saved,
+  setSaved,
+  setEdited,
+}: TipTapEditorProps) => {
   const { blog, mutate, isError, isLoading } = useBlog(
     userSlug,
     selectedBlog?.slug
   );
 
+  const saveContent = useCallback(() => {
+    if (!saved) {
+      fetch(`/api/user/${userSlug}/blog/${selectedBlog?.slug}`, {
+        method: "PUT",
+        body: JSON.stringify({
+          jsonContent: editor?.getJSON(),
+          htmlContent: editor?.getHTML(),
+        }),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          setSaved(true);
+        });
+    }
+  }, [saved]);
+
+  const handleChange = () => {
+    if (saved) {
+      setSaved(false);
+      setEdited(true);
+    }
+  };
   const editor = useEditor({
     extensions: TiptapExtensions,
     editorProps: TiptapEditorProps,
     autofocus: "end",
+    onUpdate: () => {
+      handleChange();
+    },
   });
 
   useEffect(() => {
     if (blog) {
-      console.log("Content updated");
       editor?.commands.setContent(blog.jsonContent);
     }
   }, [blog]);
+
+  useEffect(() => {
+    const autoSave = setInterval(() => {
+      saveContent();
+    }, 5 * 1000);
+    return () => {
+      clearInterval(autoSave);
+    };
+  }, [saveContent]);
 
   if (isLoading) return <LoadingCircle />;
   if (isError) return <div>Something went wrong</div>;
