1d244b4294df4c8755ab04f97f9eda33307c737e
Ankur-Datta-4 <ankurr2002@gmail.com>
2023-07-02 12:06:08 +0530

    [ADD] haul-landing

---
+++ app/auth/page.tsx
@@ -0,0 +1,9 @@
+import { LoginCard } from "./LoginCard";
+
+export default function LoginPage() {
+  return (
+    <div className="h-screen w-full flex items-center justify-center">
+      <LoginCard />
+    </div>
+  );
+}
---
+++ config/site.ts
@@ -0,0 +1,8 @@
+export const siteConfig = {
+  name: "Notion Blogger",
+  description: "A Notion clone for blogging built with NextJS13",
+  url: "https://notion-blogger.vercel.app",
+  links: {
+    github: "https://github.com/Ankur-Datta-4/blogger",
+  },
+};
